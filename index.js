5e52c006772f42ba610c0defc0d2b995e1f261a9
João Victor <elietejvc@hotmail.com>
2024-09-27 22:03:39 -0300

    Criando materias.json e adicionando na sua respectiva rota

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")//para usar o express
 const server = express()// para criar um servidor
 const copadobrasil = require("./src/times.json")
-
+const materias = require("./src/materias.json")
 server.get("/", (req, res) =>{
     return res.json({mensagem:"salve"})
 })
@@ -12,6 +12,10 @@ server.get("/corinthians", (req, res) => {
 
 server.get("/copadobrasil", (req, res) => {
     return res.json(copadobrasil)
+})
+
+server.get("/materias", (req, res) =>{
+    return res.json(materias)
 })
 
 server.listen(1910, () => {
