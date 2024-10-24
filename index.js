b18bdcd9fb987980f958c129d7aada268d7d34ed
João Victor <elietejvc@hotmail.com>
2024-10-24 20:08:52 -0300

    definindo rotas get

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express")//para usar o express
 const server = express()// para criar um servidor
 const materias = require("./src/materias.json")
+const tarefas = require("./src/tarefas.json")
 server.get("/", (req, res) =>{
     return res.json({mensagem:"salve"})
 })
 
 server.get("/materias", (req, res) =>{
     return res.json(materias)
+})
+
+server.get("/tarefas", (req, res) =>{
+    return res.json(tarefas)
 })
 
 server.get("/login/:email/:senha", (req, res) => {
