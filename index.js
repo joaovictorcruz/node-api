f4cd1ceb0f84fb74707d52bd44cee38d88f00738
João Victor <elietejvc@hotmail.com>
2024-10-24 19:58:40 -0300

    definindo rotas

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express")//para usar o express
 const server = express()// para criar um servidor
-const copadobrasil = require("./src/times.json")
 const materias = require("./src/materias.json")
 server.get("/", (req, res) =>{
     return res.json({mensagem:"salve"})
