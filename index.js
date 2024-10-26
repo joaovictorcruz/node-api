14cf2e7126cc5935541bc0a90a35028eaaff84aa
João Victor <elietejvc@hotmail.com>
2024-10-25 21:08:54 -0300

    New Branch

--- index.js
+++ index.js
@@ -1,23 +1,106 @@
-const express = require("express")//para usar o express
-const server = express()// para criar um servidor
-const materias = require("./src/materias.json")
-const tarefas = require("./src/tarefas.json")
-server.get("/", (req, res) =>{
-    return res.json({mensagem:"salve"})
+const express = require("express");
+const rotas = express();
+
+const Sequelize = require("sequelize"); 
+
+const con = new Sequelize("DiarioBordo", "root", "",{
+    host: "localhost",
+    dialect: "mysql",
+});
+
+
+const Usuario = conexaoBanco.define("usuarios",{
+    nome:{
+        type: Sequelize.STRING,
+    },
+    email:{
+        type: Sequelize.STRING,
+    },
+    senha:{
+        type: Sequelize.STRING,
+    }
 })
 
-server.get("/materias", (req, res) =>{
-    return res.json(materias)
+const PlanoEstudo = conexaoBanco.define("planoestudo",{
+    plano_titulo: {
+        type: Sequelize.STRING,
+    },
+    metas: {
+        type: Sequelize.STRING,
+    },
+    DataInicio: {
+        type: Sequelize.DATE,
+    },
+    DataFim: {
+        type: Sequelize.DATE,
+    }
 })
 
-server.get("/tarefas", (req, res) =>{
-    return res.json(tarefas)
+
+const Tarefa = conexaoBanco.define("tarefa",{
+    tarefa_titulo: {
+        type: Sequelize.STRING,
+    },
+    desc_conteudo: {
+        type: Sequelize.STRING,
+    },
+    data_vencimento:{
+        type: Sequelize.DATE,
+    },
+    horario: {
+        type: Sequelize.TIME,
+    },
+    status: {
+        type: Sequelize.STRING,
+    },
+    data_conclusao: {
+        type: Sequelize.DATE,
+    }
 })
 
-server.get("/login/:email/:senha", (req, res) => {
-    return res.json(req.params);
+const Relatorio = conexaoBanco.define("relatorio", {
+  data_relatorio: {
+    type: Sequelize.DATE,
+    tempo_gasto: Sequelize.INTEGER,
+  }
 })
 
-server.listen(1910, () => {
-    console.log("funcionando")
+/*
+Usuario.sync({ force: true});
+PlanoEstudo.sync({ force: true});
+Tarefa.sync({ force: true});
+Relatorio.sync({ force: true});
+*/
+
+con.authenticate().then(function(){
+    console.log("conexão realizada com sucesso");
+
+}).catch(function(err){
+    console.log("Erro ao conectar com o banco de dados" + err);
 })
+
+
+
+rotas.get("/", function(req, res){
+    res.send("Rota Principal");
+});
+
+rotas.get("/cadastro/:nome/:email/:senha", async function(req, res){
+    const {nome,email,senha} = req.params; //Guarda os parametros em variaveis
+
+    const novoUsuario = await Usuario.create({nome,email,senha});// insert
+
+    res.json({
+        resposta: "Usuário cadastrado com sucesso", 
+        usuario: novoUsuario
+    })
+});
+rotas.get("/exibir", async function (req, res) {
+    const usuarios = await Usuario.findAll(); // Busca todos os registros
+    res.json(usuarios); // Retorna os registros em formato JSON
+  });
+
+
+rotas.listen(1910, function () {
+    console.log("Server is running on port 1910");
+  });
